9680f640e6ce499fa0eb142108b61564f19577fe
Rom1 <romain.serres74@gmail.com>
2025-02-15 21:12:46 +0100

    Nintendo Ds
    # Conflicts:
    #	Assets/Scripts/PlayerMovement.cs
    #	Assets/prefab/Player.prefab

--- Assets/Scripts/PlayerHandler.cs
+++ Assets/Scripts/PlayerHandler.cs
@@ -1,16 +1,25 @@
+using System;
+using System.Collections;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class PlayerHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Animator animator;
+    private bool dsIsOpen = false;
+    [SerializeField] private GameObject NintendoDs;
+    [SerializeField] private float moveDuration = 0.5f; // Temps pour l'animation
+
+    private Vector3 dsOpenPosition;
+    private Vector3 dsClosedPosition;
+
     void Start()
     {
-    }
+        animator = GetComponentInChildren<Animator>();
 
-    // Update is called once per frame
-    void Update()
-    {
-
+        // Définir les positions d'ouverture et de fermeture
+        dsOpenPosition = NintendoDs.transform.position;
+        dsClosedPosition = dsOpenPosition + new Vector3(0, -2f, 0);
     }
 
     public void SetActive(bool active)
@@ -18,5 +27,44 @@ public class PlayerHandler : MonoBehaviour
         GetComponentInChildren<Camera>().enabled = active;
         GetComponentInChildren<AudioListener>().enabled = active;
         GetComponent<PlayerMovement>().enabled = active;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log("DS");
+            if (dsIsOpen)
+            {
+                Debug.Log("Closing");
+                StartCoroutine(MoveDs(dsClosedPosition));
+                animator.SetBool("isClosing", true);
+                animator.SetBool("isOpening", false);
+                dsIsOpen = false;
+            }
+            else
+            {
+                Debug.Log("Opening");
+                StartCoroutine(MoveDs(dsOpenPosition));
+                animator.SetBool("isOpening", true);
+                animator.SetBool("isClosing", false);
+                dsIsOpen = true;
+            }
+        }
+    }
+
+    private IEnumerator MoveDs(Vector3 targetPosition)
+    {
+        float elapsedTime = 0f;
+        Vector3 startPosition = NintendoDs.transform.position;
+
+        while (elapsedTime < moveDuration)
+        {
+            NintendoDs.transform.position = Vector3.Lerp(startPosition, targetPosition, elapsedTime / moveDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null; // Attendre la prochaine frame
+        }
+
+        NintendoDs.transform.position = targetPosition; // S'assurer qu'on atteint bien la position cible
     }
 }
